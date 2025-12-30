2c893b3b3c472f3b9f6fd49ee98b3efb66616f28
lytvyniuk_v <lv@pinpools.com>
2025-12-30 21:26:28 +0100

    First steps

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import Header from '../appHeader/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/appHeader/Header.js
@@ -0,0 +1,16 @@
+import './Header.scss';
+
+function Header() {
+    return (
+        <div className="header">
+            <div>
+                <span className="red-text">Marvel</span> information portal
+            </div>
+            <div>
+                <span className="red-text">Characters</span> / Comics
+            </div>
+        </div>
+    );
+}
+
+export default Header;
